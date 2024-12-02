f405934f7ede32845c905985db14b33913bc1f75
robfil50219 <robfil50219@stud.noroff.no>
2024-12-02 15:03:38 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Link to="/">ShopSphere</Link>
+        <Link to="/cart">Cart</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
 
