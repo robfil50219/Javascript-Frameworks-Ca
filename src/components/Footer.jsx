e421f35bd282312814a7006a5f9027b5a92edc88
robfil50219 <robfil50219@stud.noroff.no>
2024-12-02 15:04:12 +0100

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} ShopSphere</p>
+    </footer>
+  );
+};
+
+export default Footer;
